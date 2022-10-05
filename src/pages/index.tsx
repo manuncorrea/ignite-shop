5f1f75c8ec19815e03a646a59c6c5ee9e18d3796
Emanuele Correa <manuncorrea@hotmail.com>
2022-10-05 15:10:54 -0300

    configurando o stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocktseat',
+  borderRadius: 6,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>hello bay</h1>
+    <Button>Enviary <span>teste</span></Button>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import {createStitches} from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      rocktseat: '#8257e6'
+    }
+  }
+})
+
