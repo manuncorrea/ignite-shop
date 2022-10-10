ec68bf84783c3651a286ca325872bcf55000ea21
Emanuele Correa <manuncorrea@hotmail.com>
2022-10-10 14:50:19 -0300

    navegação de link no next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Image from "next/future/image"
 
-import { GetStaticProps } from "next"
 import { useKeenSlider } from 'keen-slider/react'
+import { GetStaticProps } from "next"
+import Link from 'next/link'
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import Stripe from "stripe"
@@ -30,14 +31,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" /> 
+          <Link href={`/product/${product.id}`} key={product.id} >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" /> 
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
       
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -45,7 +45,8 @@ export const Product = styled('a', {
     background: 'rgba(0, 0, 0, 0.6)',
 
     strong: {
-      fontSize: '$lg'
+      fontSize: '$lg',
+      color: '$gray100'
     },
 
     span: {
