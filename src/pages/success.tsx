8c6c8b498182e2d482d8236f46a98c109292afa4
Emanuele Correa <manuncorrea@hotmail.com>
2022-10-10 18:47:27 -0300

    redirect no ssr

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -34,8 +34,16 @@ export default function Suscess({costumerName, product}: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if(!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
   const sessionId = String(query.session_id);
-
+  
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ['line_items', 'line_items.data.price.product']
   });
