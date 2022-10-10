f1e974e67a5ccf3ac81460481892fb1107344ec4
Emanuele Correa <manuncorrea@hotmail.com>
2022-10-10 18:23:20 -0300

    estruturando a pg de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Suscess() {
   return(
-    <h1>Suscess</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Emanuele Correa</strong>, sua <strong>Camiseta Bayond the limits</strong> já está a caminho de sua casa
+      </p>
+
+      <Link href="/"> 
+        Voltar ao catálago
+      </Link>
+    </SuccessContainer>
   )
 }
